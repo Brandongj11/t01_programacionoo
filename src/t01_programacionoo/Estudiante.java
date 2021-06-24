5d698347eca8de33beff1330c83bc31df9f758d4
BRANDON <BRANDON@TRABAJO>
2021-06-24 15:58:14 -0500

    Ejemplos de Encapsulamiento,Abstraccion,Polimorfismo y Herencia

--- src/t01_programacionoo/Estudiante.java
+++ src/t01_programacionoo/Estudiante.java
@@ -1,5 +1,20 @@
 package t01_programacionoo;
 
 public class Estudiante {
-private String nombre ;
+	/* Encapsulacion */
+	private String nombre;
+	private String apellido;
+
+	/* Abstraccion */
+	void aprender(String Matematicas, String suma) {
+		int estudiar = 0;
+	}
+
+	/* Polimorfismo */
+	void aprender(String Historia) {
+		int estudiar = 0;
+		String materia = Historia;
+	}
+	/*Herencia*/
+	
 }
---
+++ src/t01_programacionoo/Materia.java
@@ -0,0 +1,13 @@
+package t01_programacionoo;
+
+public class Materia {
+	private String nombre;
+
+	void estudiarprueba() {
+		Estudiante estudiante = new Estudiante();
+		estudiante.aprender();
+	}
+
+	void aprender() {
+	}
+}
