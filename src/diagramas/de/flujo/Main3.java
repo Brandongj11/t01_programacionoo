74b6e7cd56b3188e3d4359083b467ecb105394f9
BRANDON <BRANDON@TRABAJO>
2021-07-09 19:40:47 -0500

    Tabla

---
+++ src/diagramas/de/flujo/Main3.java
@@ -0,0 +1,15 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		for (int n = 0; n <= 12; n++) {
+			int d = 7 * n;
+
+			System.out.println("7x" + n + "=" + d);
+
+		}
+	}
+}
