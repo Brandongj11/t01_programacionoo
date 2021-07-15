601383221745ada139618560048ff0c15ee18e1b
BRANDON <BRANDON@TRABAJO>
2021-07-14 19:12:49 -0500

    Taller 3 y 4

---
+++ src/diagramas/de/flujo/Mainfigurainv.java
@@ -0,0 +1,20 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class Mainfigurainv {
+
+	public static void main(String[] args) {
+		System.out.print("Ingrese el numero de filas:");
+		Scanner teclado = new Scanner(System.in);
+		int n = teclado.nextInt();
+		for (int fila = 1; fila <= n; fila++) {
+			for (int ast = n; ast >= fila; ast--) {
+				System.out.print("*");
+			}
+			System.out.println();//Salto de linea 
+		}
+	}
+
+	}
+
