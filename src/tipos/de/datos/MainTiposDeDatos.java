a9acd5d849a527ac0b618f9bd5a29068fe6c4c3c
BRANDON <BRANDON@TRABAJO>
2021-07-21 18:51:12 -0500

    Taller Tipos

---
+++ src/tipos/de/datos/MainTiposDeDatos.java
@@ -0,0 +1,19 @@
+package tipos.de.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		short cantidadEstudiatesUCE = 1000;
+		byte entradaCircuito= 0;
+		int cantCiudadanos = 100000;
+		long cantidadMundo = 1000000000;
+		float sueldo = 10.56F;
+		double saldoBanco = 10000000.67;
+		char opcionMenu = 'C' ;
+		boolean casado = true;
+		
+		
+
+	}
+
+}
