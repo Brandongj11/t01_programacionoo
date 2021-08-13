2d20cacfa644992aac6a478b13ca24f2bdee1248
BRANDON <BRANDON@TRABAJO>
2021-08-13 12:51:55 -0500

    Trabajo metodos

---
+++ src/trabajo/metodos/PrecioEntrada.java
@@ -0,0 +1,11 @@
+package trabajo.metodos;
+
+public class PrecioEntrada {
+
+	public static void main(String[] args) {
+		Cliente cliente1 = new Cliente();
+		cliente1.Cliente("Jhonny Ninabanda", 1728043421);
+
+	}
+
+}
