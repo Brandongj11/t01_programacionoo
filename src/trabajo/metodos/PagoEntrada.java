80d696edab647ff9b9465d3345feca5072e3a739
BRANDON <BRANDON@TRABAJO>
2021-08-13 13:16:38 -0500

    Trabajo metodos

---
+++ src/trabajo/metodos/PagoEntrada.java
@@ -0,0 +1,10 @@
+package trabajo.metodos;
+
+public class PagoEntrada {
+
+	public static void main(String[] args) {
+		Cliente cliente1 = new Cliente();
+		cliente1.Cliente("Jhonny Ninabanda", 172803421);
+	}
+
+}
